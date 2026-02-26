917c1d8614950e0b1b034ede6faed2440ad5491f
epdlrnldudnj <kje0023@naver.com>
2026-02-26 19:31:53 +0900

    fix: ci 실패 해결

--- src/main/java/com/example/kloset_lab/global/config/KafkaConsumerConfig.java
+++ src/main/java/com/example/kloset_lab/global/config/KafkaConsumerConfig.java
@@ -7,11 +7,13 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
 
 @Configuration
+@Profile("!test")
 public class KafkaConsumerConfig {
 
     @Value("${spring.kafka.bootstrap-servers}")
