ca81635a78df7512344fa4feac50e5404ea79e44
epdlrnldudnj <kje0023@naver.com>
2026-02-27 19:33:35 +0900

    fix: cd 재실행용

--- src/main/java/com/example/kloset_lab/global/ai/kafka/producer/ClothesAnalysisProducer.java
+++ src/main/java/com/example/kloset_lab/global/ai/kafka/producer/ClothesAnalysisProducer.java
@@ -16,7 +16,6 @@ import org.springframework.stereotype.Component;
 public class ClothesAnalysisProducer {
 
     private static final String TOPIC = "ai.clothes.analyze.request";
-
     @Qualifier("clothesAnalysisKafkaTemplate") private final KafkaTemplate<String, Object> kafkaTemplate;
 
     /**
