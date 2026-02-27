251a00b0c8f038239c58845c99c1ffb411147247
epdlrnldudnj <kje0023@naver.com>
2026-02-27 22:31:45 +0900

    fix: ci cd 재시도

--- src/main/java/com/example/kloset_lab/global/config/SwaggerConfig.java
+++ src/main/java/com/example/kloset_lab/global/config/SwaggerConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile({"local", "dev"})
+@Profile({"dev", "local"})
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
