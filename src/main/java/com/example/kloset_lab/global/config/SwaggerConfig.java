72179779d4def2f5d2b2d14b87512fc32f38d658
epdlrnldudnj <kje0023@naver.com>
2026-02-27 22:21:10 +0900

    feat: SwaggerConfig 추가

---
+++ src/main/java/com/example/kloset_lab/global/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.example.kloset_lab.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile({"local", "dev"})
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components()
+                .addSecuritySchemes(
+                        jwt,
+                        new SecurityScheme()
+                                .name(jwt)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT"));
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+
+    private Info apiInfo() {
+        return new Info().title("kloset lab server").description("kloset lab 백엔드 api 테스트 입니다");
+    }
+}
